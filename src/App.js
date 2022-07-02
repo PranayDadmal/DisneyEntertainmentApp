db2510777b8db9a90a808de2ace3abce65183d09
Pranay Dadmal <pranaydadmal29@gmail.com>
2022-07-02 17:20:55 +0530

    header, footer and navigation are completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { Container } from '@mui/system';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+import Trending from './pages/Trending/Trending';
+import Movies from './pages/Movies/Movies';
+import Series from './pages/Series/Series';
+import Search from './pages/Search/Search';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+  
+   <BrowserRouter>
+      <Header />
+      <div className="app"> 
+        <Container>
+         <Routes>
+          <Route path="/" element={<Trending/>}  exact />
+          <Route path="/movies" element={<Movies/>}  />
+          <Route path="/series" element={<Series/>} />
+          <Route path="/search" element={<Search/>} />
+         </Routes>
+        </Container>
+     </div>
+     <SimpleBottomNavigation />
+   </BrowserRouter>
+  
+    
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const  Header = ()  => {
+  return (
+       <span className="header">DISNEY WORLD</span>
+    )
+}
+
+export default Header;
